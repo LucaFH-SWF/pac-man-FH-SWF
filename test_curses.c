3e0efc22c26747d69a464a9b4640eea75cbefd8a
Luca G <goedeker.luca@fh-swf.de>
2023-12-03 21:05:15 +0100

    2023-12-03_21:02:27

--- test_curses.c
+++ test_curses.c
@@ -28,16 +28,16 @@ int main()
 
 	char **points;
 
-	points = (char **) malloc(sizex * sizeof(points)); //sizey viele pointer auf pointer
+	points = (char **) malloc(sizey * sizeof(points)); //sizey viele pointer auf pointer
 
-	for(int i = 0; i < sizex; ++i) //sizex viele char reservieren für jeden pointer
-		points[i] = (char *) malloc(sizey * sizeof(char));
+	for(int i = 0; i < sizey; ++i) //sizex viele char reservieren für jeden pointer
+		points[i] = (char *) malloc(sizex * sizeof(char));
 
-	for(int i = 0; i < sizex; ++i)
+	for(int i = 0; i < sizey; ++i)
 	{
-		for(int j = 0; j < sizey; ++j)
+		for(int j = 0; j < sizex; ++j)
 		{
-			points[i][j] = 0;
+			points[j][i] = '\0';
 		}
 	}
 	points[1][2] = '.';
