9d20fd66ff0be922a921e4b3843bf4c8f3f008d7
Luca G <goedeker.luca@fh-swf.de>
2023-12-03 16:55:40 +0100

    2023-12-03_16:43:19

--- test_curses.c
+++ test_curses.c
@@ -1,8 +1,17 @@
 #include <ncurses.h>
+#include <stdlib.h>
+
+void print_src(char *walls, int sizex, int sizey);
 
 int main()
 {
 	int quit = 1;
+	int sizex = 10;
+	int sizey = 20;
+	
+	char *walls = (char *) malloc(sizex*sizey * sizeof(char));
+
+	walls[1][2] = 'a';
 
 	initscr();
 	cbreak();
@@ -11,12 +20,25 @@ int main()
 	nodelay(stdscr, TRUE);
 
 	curs_set(0);
-
+/*
 	while(!quit)//action-loop
 	{
 		
 	}
+*/
+	print_src(walls, sizex, sizey);
 
 	endwin();
 	return 0;
+}
+
+void print_src(char *walls, int sizex, int sizey)
+{
+	for(int i = 0; i < sizey; ++i)
+	{
+		for(int j = 0; j < sizex; ++j)
+		{
+			mvprintw(5+i, 5+j, "%c", walls[j][i]);
+		}
+	}
 }
