1b8e956552e0213f99830d349a9187cc534896d0
Luca G <goedeker.luca@fh-swf.de>
2023-12-03 20:53:16 +0100

    2023-12-03_20:51:19

--- test_curses.c
+++ test_curses.c
@@ -20,7 +20,7 @@ int main()
 	{
 		for(int j = 0; j < sizex; ++j)
 		{
-			walls[i][j] = 'a';
+			walls[i][j] = 'W';
 		}
 	}
 	
@@ -33,6 +33,9 @@ int main()
 	nodelay(stdscr, TRUE);
 
 	curs_set(0);
+
+	init_pair(1, COLOR_WHITE, COLOR_BLUE);
+	
 /*
 	while(!quit)//action-loop
 	{
@@ -50,6 +53,7 @@ int main()
 
 void print_src(char **walls, int sizex, int sizey)
 {
+	attron(COLOR_PAIR(1));
 	for(int i = 0; i < sizey; ++i)
 	{
 		for(int j = 0; j < sizex; ++j)
@@ -57,4 +61,5 @@ void print_src(char **walls, int sizex, int sizey)
 			mvprintw(5+i, 5+j, "%c", walls[i][j]);
 		}
 	}
+	attroff(COLOR_PAIR(1));
 }
